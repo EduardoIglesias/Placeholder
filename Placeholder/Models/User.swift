502edad92b29a67186d80b9691c7c1e3c60d4aa3
Eduardo Iglesias <eiglesiasf@viewnext.com>
2020-06-09 19:39:56 +0200

    Added nested data in User Model

--- Placeholder/Models/User.swift
+++ Placeholder/Models/User.swift
@@ -13,26 +13,30 @@ struct User: Decodable {
     let name: String?
     let username: String?
     let email: String?
+    let address: Address
     let phone: String?
     let website: String?
+    let company: Company
     
-//    struct address {
-//        let street: String
-//        let suite: String
-//        let city: String
-//        let zipcode: String
-//        
-//        struct geo {
-//            let lat: String
-//            let lng: String
-//        }
-//    }
-//    
-//    struct company {
-//        let name: String
-//        let catchPhrase: String
-//        let bs: String
-//    }
+    struct Address: Decodable {
+        let street: String
+        let suite: String
+        let city: String
+        let zipcode: String
+        let geo: Geo
+
+    }
+    
+    struct Geo: Decodable {
+        let lat: String
+        let lng: String
+    }
+    
+    struct Company: Decodable {
+        let name: String
+        let catchPhrase: String
+        let bs: String
+    }
     
 }
 
